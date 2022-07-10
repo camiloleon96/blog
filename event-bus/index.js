0afdb4594ee223f33991eb8e23769ed21aadc1c7
camiloleone96 <juanc.leon96@gmail.com>
2022-07-10 17:17:35 +0200

    complete simple quey service

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,15 +6,17 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/events', async (req,res) => {
+    console.log('_________________________')
     const event = req.body;
     console.warn(event);
     try {
         await axios.post('http://localhost:4000/events', event);
         await axios.post('http://localhost:4001/events', event);
-        //await axios.post('http://localhost:4002/events', event);
-
+        await axios.post('http://localhost:4002/events', event);
+        console.warn('----ALL ok----');
         res.send({status: 'ok'});    
     } catch (error) {
+        console.log('---ERROR---')
         console.warn(error.message)
     }
     
