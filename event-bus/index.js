8fc0714a8394510aee1a717c3e33633c5f8b57dc
camiloleone96 <juanc.leon96@gmail.com>
2022-07-10 12:34:37 +0200

    create event emmiters inside the comments and posts services + lint errors

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,17 +5,21 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (req,res) => {
+app.post('/events', async (req,res) => {
     const event = req.body;
+    console.warn(event);
+    try {
+        //await axios.post('http://localhost:4000/events', event);
+        //await axios.post('http://localhost:4001/events', event);
+        //await axios.post('http://localhost:4002/events', event);
 
-    axios.post('http//localhost:4000/events', events);
-    axios.post('http//localhost:4001/events', events);
-    axios.post('http//localhost:4002/events', events);
-
-    res.send({status: 'ok'});
-
+        res.send({status: 'ok'});    
+    } catch (error) {
+        console.warn(error.message)
+    }
+    
 });
 
 app.listen(4005, () => {
-    console.log('Listening on port 4005')
+    console.warn('Listening on port 4005');
 });
--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 
 const app = express();
@@ -14,15 +15,27 @@ app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
-    const id = randomBytes(4).toString('Hex');//4 hexa Bytes
-    const {title} = req.body;
+app.post('/posts', async (req, res) => {
+    try {
+        const id = randomBytes(4).toString('Hex');//4 hexa Bytes
+        const {title} = req.body;
 
-    posts[id] = {
-        id, title
-    };
+        posts[id] = {
+            id, title
+        };
 
-    res.status(201).send(posts[id]);
+        await axios.post('http://localhost:4005/events', {
+            type: "PostCreated",
+            data: {
+                id, title
+            }
+        });
+
+        res.status(201).send(posts[id]);    
+    } catch (error) {
+        console.warn(error.message);        
+    }
+    
 });
 
 app.listen(4000, () => {
